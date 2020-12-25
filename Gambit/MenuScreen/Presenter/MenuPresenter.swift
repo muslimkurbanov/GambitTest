d6f46526420fb67486b043a2ec9b490d42fbe0c6
muslimkurbanov <vrog96@mail.ru>
2020-12-25 21:55:50 +0300

    Move-statusBarColorFunction-to-presenter

--- Gambit/MenuScreen/Presenter/MenuPresenter.swift
+++ Gambit/MenuScreen/Presenter/MenuPresenter.swift
@@ -6,13 +6,58 @@
 //
 
 import Foundation
+import UIKit
 
 protocol MenuPresenterProtocol: class {
     init(view: MenuProtocol)
     func getMenu()
+    func addItemCenter(view: UIView, item: UINavigationItem)
 }
 
 class MenuPresenter: MenuPresenterProtocol {
+    var menuView = UIView()
+    var navigationItem = UINavigationItem()
+
+    func addItemCenter(view: UIView, item: UINavigationItem) {
+        let image = #imageLiteral(resourceName: "gambit")
+        let imageView = UIImageView()
+        self.menuView = view
+        self.navigationItem = item
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalToConstant: 19).isActive = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = image
+        
+        let contentView = UIView()
+        self.navigationItem.titleView = contentView
+        self.navigationItem.titleView?.addSubview(imageView)
+        imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        
+        if #available(iOS 13.0, *) {
+            let app = UIApplication.shared
+            let statusBarHeight: CGFloat = app.statusBarFrame.size.height
+            
+            let statusbarView = UIView()
+            statusbarView.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.07058823529, blue: 0.5607843137, alpha: 1)
+            view.addSubview(statusbarView)
+          
+            statusbarView.translatesAutoresizingMaskIntoConstraints = false
+            statusbarView.heightAnchor
+                .constraint(equalToConstant: statusBarHeight).isActive = true
+            statusbarView.widthAnchor
+                .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+            statusbarView.topAnchor
+                .constraint(equalTo: view.topAnchor).isActive = true
+            statusbarView.centerXAnchor
+                .constraint(equalTo: view.centerXAnchor).isActive = true
+          
+        } else {
+            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
+            statusBar?.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.07058823529, blue: 0.5607843137, alpha: 1)
+        }
+    }
+    
     
     
     private weak var view: MenuProtocol?
--- Gambit/MenuScreen/View/MenuViewController.swift
+++ Gambit/MenuScreen/View/MenuViewController.swift
@@ -44,48 +44,7 @@ class MenuViewController: UIViewController {
         menuTableView.delegate = self
         menuTableView.dataSource = self
         
-        let image = #imageLiteral(resourceName: "gambit")
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.heightAnchor.constraint(equalToConstant: 19).isActive = true
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = image
-        
-        //imageView.backgroundColor = .lightGray
-        
-        // In order to center the title view image no matter what buttons there are, do not set the
-        // image view as title view, because it doesn't work. If there is only one button, the image
-        // will not be aligned. Instead, a content view is set as title view, then the image view is
-        // added as child of the content view. Finally, using constraints the image view is aligned
-        // inside its parent.
-        let contentView = UIView()
-        self.navigationItem.titleView = contentView
-        self.navigationItem.titleView?.addSubview(imageView)
-        imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-        
-        if #available(iOS 13.0, *) {
-            let app = UIApplication.shared
-            let statusBarHeight: CGFloat = app.statusBarFrame.size.height
-            
-            let statusbarView = UIView()
-            statusbarView.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.07058823529, blue: 0.5607843137, alpha: 1)
-            view.addSubview(statusbarView)
-          
-            statusbarView.translatesAutoresizingMaskIntoConstraints = false
-            statusbarView.heightAnchor
-                .constraint(equalToConstant: statusBarHeight).isActive = true
-            statusbarView.widthAnchor
-                .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-            statusbarView.topAnchor
-                .constraint(equalTo: view.topAnchor).isActive = true
-            statusbarView.centerXAnchor
-                .constraint(equalTo: view.centerXAnchor).isActive = true
-          
-        } else {
-            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
-            statusBar?.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.07058823529, blue: 0.5607843137, alpha: 1)
-        }
+        presenter.addItemCenter(view: view, item: navigationItem)
     }
     // Do any additional setup after loading the view.
 }
